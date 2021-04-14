3f4b12eef42c23f9b5a64f63a928e265203fe005
CheeKiam Ng <cheekiam.ng@weareeverise.com>
2021-04-14 22:57:16 +0800

    init dispatcher sample

---
+++ src/components/ToDoList.tsx
@@ -0,0 +1,39 @@
+import * as React from "react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import { filteredTodoListState } from "../selectors";
+import { TodoItemCreator } from "./ToDoItemCreator";
+import { TodoListItem } from "./ToDoListItem";
+import { TodoListStats } from "./ToDoListStats";
+import { TodoListFilters } from "./ToDoListFilters";
+import { RecycleBinList } from "./RecycleBinList";
+import { CreateDispatcher, Dispatcher } from "../dispatcher";
+import { dispatcherState } from "../atoms";
+import { Log } from "./Log";
+
+export const ToDoList = () => {
+    const todoList = useRecoilValue(filteredTodoListState);
+    const setDispatcher = useSetRecoilState(dispatcherState);
+
+    const dispatcherRef = React.useRef<Dispatcher>(CreateDispatcher());
+    React.useEffect(() => {
+      setDispatcher(dispatcherRef.current);
+    }, []);
+
+    return (
+        <>
+          <TodoListStats />
+          <TodoListFilters />
+          <TodoItemCreator />
+    
+          {todoList.map((todoItem) => (
+            <TodoListItem key={todoItem.id} item={todoItem} />
+          ))}
+
+          <hr/>
+          <RecycleBinList />
+
+          <hr/>
+          <Log />
+        </>
+      );
+};
